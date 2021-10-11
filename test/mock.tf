f63e456c8ae3819a74ddc3da0e29cb16d51401bf
morsecodist <todd@morsecodist.io>
2021-10-11 14:03:40 -0400

    localstack

--- test/mock.tf
+++ test/mock.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   endpoints {
-    stepfunctions    = "http://localhost:8083"
+    stepfunctions    = "http://localhost:9000"
     batch            = "http://localhost:9000"
     iam              = "http://localhost:9000"
     ec2              = "http://localhost:9000"
