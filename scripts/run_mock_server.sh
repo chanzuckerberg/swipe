e757d67b4a023f81009305ed15bb4750c44d5698
morsecodist <todd@morsecodist.io>
2021-10-11 16:14:36 -0400

    docker-compose

--- scripts/run_mock_server.sh
+++ scripts/run_mock_server.sh
@@ -9,3 +9,5 @@ docker pull amazon/aws-stepfunctions-local
 # moto_server --host 0.0.0.0 --port 4566 &
 
 # docker run --network host -e BATCH_ENDPOINT=http://localhost:4566 -e LAMBDA_ENDPOINT=http://localhost:4566 -e AWS_ACCOUNT_ID=123456789012 amazon/aws-stepfunctions-local
+
+docker-compose up -d
