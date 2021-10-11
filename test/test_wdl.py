ef18832472651b2b8dfd6410da94f7e70177a9ad
morsecodist <todd@morsecodist.io>
2021-10-11 13:31:45 -0400

    exp

--- test/test_wdl.py
+++ test/test_wdl.py
@@ -29,7 +29,7 @@ class TestSFNWDL(unittest.TestCase):
         self.s3 = boto3.resource("s3", endpoint_url="http://localhost:9000")
         self.sfn = boto3.client("stepfunctions", endpoint_url="http://localhost:8083")
         self.test_bucket = self.s3.create_bucket(Bucket="swipe-test")
-        self.lamb = boto3.resource("lambda", endpoint_url="http://localhost:9000")
+        self.lamb = boto3.client("lambda", endpoint_url="http://localhost:9000")
 
     def test_simple_sfn_wdl_workflow(self):
         response = self.lamb.invoke(
