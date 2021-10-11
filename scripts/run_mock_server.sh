4cf28432c2b9dfdcb58e72176d19ccb412040591
morsecodist <todd@morsecodist.io>
2021-10-11 11:00:55 -0400

    dumb networking

--- scripts/run_mock_server.sh
+++ scripts/run_mock_server.sh
@@ -8,4 +8,4 @@ docker pull amazon/aws-stepfunctions-local
 
 moto_server --host 0.0.0.0 --port 9000 &
 
-docker run -p 8083:8083 -e BATCH_ENDPOINT=http://127.0.0.1:9000 -e LAMBDA_ENDPOINT=http://127.0.0.1:9000 -e AWS_ACCOUNT_ID=123456789012 amazon/aws-stepfunctions-local
+docker run --network host -e BATCH_ENDPOINT=http://localhost:9000 -e LAMBDA_ENDPOINT=http://localhost:9000 -e AWS_ACCOUNT_ID=123456789012 amazon/aws-stepfunctions-local
