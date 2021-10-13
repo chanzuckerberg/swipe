897db36f0b6df7ece1ae2aca75b2f77f3f47f894
morsecodist <todd@morsecodist.io>
2021-10-13 19:10:52 -0400

    motoserver

--- scripts/run_mock_server.sh
+++ scripts/run_mock_server.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 trap "exit" INT TERM
 trap "kill 0" EXIT
 
-moto_server --host 0.0.0.0 --port 9000 &
+docker run -p 9000:5000 -d motoserver/moto
 
 docker run --network host -e BATCH_ENDPOINT=http://localhost:9000 -e LAMBDA_ENDPOINT=http://localhost:9000 -e AWS_ACCOUNT_ID=123456789012 amazon/aws-stepfunctions-local
