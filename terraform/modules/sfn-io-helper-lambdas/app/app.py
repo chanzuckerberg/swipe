43c6c905edede220bc8b4ac99e09a3ae5a2efd5a
morsecodist <todd@morsecodist.io>
2021-10-13 14:54:47 -0400

    lambda commit

--- terraform/modules/sfn-io-helper-lambdas/app/app.py
+++ terraform/modules/sfn-io-helper-lambdas/app/app.py
@@ -40,7 +40,7 @@ logging.getLogger().setLevel(logging.INFO)
 
 
 def preprocess_input(sfn_data, _):
-    s3 = boto3.client('s3', endpoint_url=os.getenv("AWS_ENDPOINT_URL"))
+    s3 = boto3.client('s3', endpoint_url=os.getenv("AWS_ENDPOINT_URL"), region_name=os.getenv("AWS_DEFAULT_REGION"))
     response = s3.list_buckets()
 
     # Output the bucket names
