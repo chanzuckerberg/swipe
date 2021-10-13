9c5e6ca68ff2192a9e01eb6b089e04cf5a2841b6
morsecodist <todd@morsecodist.io>
2021-10-12 23:19:50 -0400

    set back

--- scripts/run_mock_server.sh
+++ scripts/run_mock_server.sh
@@ -6,15 +6,4 @@ trap "kill 0" EXIT
 
 moto_server --host 0.0.0.0 --port 9000 &
 
-docker run \
-    -d \
-    --network host \
-    -e SERVICES=lambda \
-    -e DEFAULT_REGION=$AWS_DEFAULT_REGION \
-    -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
-    -e DEBUG=1 \
-    -e LAMBDA_EXECUTOR=local \
-    -e EDGE_PORT=9001 \
-    localstack/localstack lambda
-
-docker run --network host -e BATCH_ENDPOINT=http://localhost:9000 -e LAMBDA_ENDPOINT=http://localhost:9001 -e AWS_ACCOUNT_ID=123456789012 amazon/aws-stepfunctions-local
+docker run --network host -e BATCH_ENDPOINT=http://localhost:9000 -e LAMBDA_ENDPOINT=http://localhost:9000 -e AWS_ACCOUNT_ID=123456789012 amazon/aws-stepfunctions-local
