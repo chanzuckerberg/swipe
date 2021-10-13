da609bfb1deb4be338cdf8435c82a9bd4dddf059
morsecodist <todd@morsecodist.io>
2021-10-13 19:20:24 -0400

    compose

--- scripts/run_mock_server.sh
+++ scripts/run_mock_server.sh
@@ -4,6 +4,4 @@ set -euo pipefail
 trap "exit" INT TERM
 trap "kill 0" EXIT
 
-docker run -p 9000:5000 -d motoserver/moto
-
-docker run --network host -e BATCH_ENDPOINT=http://localhost:9000 -e LAMBDA_ENDPOINT=http://localhost:9000 -e AWS_ACCOUNT_ID=123456789012 amazon/aws-stepfunctions-local
+docker-compose up
