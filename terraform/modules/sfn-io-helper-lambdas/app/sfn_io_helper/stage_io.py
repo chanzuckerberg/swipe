c0594727992036e6dac1a8a6fd9b62147af0388a
morsecodist <todd@morsecodist.io>
2021-10-12 20:17:59 -0400

    lambda commit

--- terraform/modules/sfn-io-helper-lambdas/app/sfn_io_helper/stage_io.py
+++ terraform/modules/sfn-io-helper-lambdas/app/sfn_io_helper/stage_io.py
@@ -25,7 +25,6 @@ def get_stage_input(sfn_state, stage):
 
 def put_stage_input(sfn_state, stage, stage_input):
     input_uri = sfn_state[get_input_uri_key(stage)]
-    raise Exception(input_uri)
     s3_object(input_uri).put(Body=json.dumps(stage_input).encode())
 
 
