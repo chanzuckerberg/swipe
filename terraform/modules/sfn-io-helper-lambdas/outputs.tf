a40853919593a2628b3a01bf98d785bd593ded3c
morsecodist <todd@morsecodist.io>
2021-10-14 17:57:53 -0400

    proper outputs

--- terraform/modules/sfn-io-helper-lambdas/outputs.tf
+++ terraform/modules/sfn-io-helper-lambdas/outputs.tf
@@ -1,15 +1,15 @@
 output "preprocess_input_lambda_name" {
-  value = aws_lambda_function.lambda["preprocess_input"].name
+  value = aws_lambda_function.lambda["preprocess_input"].function_name
 }
 
 output "process_stage_output_lambda_name" {
-  value = aws_lambda_function.lambda["process_stage_output"].name
+  value = aws_lambda_function.lambda["process_stage_output"].function_name
 }
 
 output "handle_success_lambda_name" {
-  value = aws_lambda_function.lambda["handle_success"].name
+  value = aws_lambda_function.lambda["handle_success"].function_name
 }
 
 output "handle_failure_lambda_name" {
-  value = aws_lambda_function.lambda["handle_failure"].name
+  value = aws_lambda_function.lambda["handle_failure"].function_name
 }
