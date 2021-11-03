b39a7b15896349051eeb901c8d43fa43684ea8b9
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2021-11-03 16:16:57 -0400

    Remove variable

--- variables.tf
+++ variables.tf
@@ -10,12 +10,6 @@ variable "OWNER" {
   type = string
 }
 
-# must be optional
-variable "BATCH_SSH_PUBLIC_KEY" {
-  type = string
-  default = null
-}
-
 variable "batch_security_group_ids" {
   description = "EC2 security group IDs for Batch EC2 compute environment container instances"
   type        = list(string)
