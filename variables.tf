6b520ff14b7a7a150d8e2f5f36ec151c30c8dd99
morsecodist <todd@morsecodist.io>
2021-11-08 11:01:59 -0500

    ssh key add back

--- variables.tf
+++ variables.tf
@@ -10,6 +10,11 @@ variable "OWNER" {
   type = string
 }
 
+variable "BATCH_SSH_PUBLIC_KEY" {
+  type    = string
+  default = ""
+}
+
 variable "batch_security_group_ids" {
   description = "EC2 security group IDs for Batch EC2 compute environment container instances"
   type        = list(string)
