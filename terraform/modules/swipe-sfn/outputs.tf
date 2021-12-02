4a869f6cb0cda15503a7c19228d4f94527821d6f
morsecodist <todd@morsecodist.io>
2021-12-01 23:14:05 -0500

    exp

--- terraform/modules/swipe-sfn/outputs.tf
+++ terraform/modules/swipe-sfn/outputs.tf
@@ -1,3 +1,3 @@
 output "sfn_arns" {
-  value = { for sfn in aws_sfn_state_machine.swipe_single_wdl : sfn.key => "test" }
+  value = { for name, sfn in aws_sfn_state_machine.swipe_single_wdl : name => sfn.arn }
 }
