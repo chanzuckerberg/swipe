6ce48a818f51b47e785d47daae5e64ae664a4f3d
morsecodist <todd@morsecodist.io>
2021-12-08 16:47:14 -0500

    input

--- terraform/modules/sfn-io-helper-lambdas/app/sfn_io_helper/stage_io.py
+++ terraform/modules/sfn-io-helper-lambdas/app/sfn_io_helper/stage_io.py
@@ -46,6 +46,7 @@ mappy_map = {
         "contig_in_contigs_fasta": "assembly_out_assembly_contigs_fasta",
         "fastqs_0": ["HostFilter", "fastqs_0"],
         "fastqs_1": ["HostFilter", "fastqs_1"],
+        "nonhost_fasta_refined_taxid_annot_fasta": "refined_taxid_fasta_out_assembly_refined_taxid_annot_fasta",
         "duplicate_clusters_csv": "idseq_dedup_out_duplicate_cluster_sizes_tsv",
     },
 }
