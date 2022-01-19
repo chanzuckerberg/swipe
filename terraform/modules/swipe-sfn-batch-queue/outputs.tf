0f70f2a122cc61777e34c0a54b1cee251b9b6def
morsecodist <todd@morsecodist.io>
2022-01-18 22:06:07 -0500

    output batch CE security group

--- terraform/modules/swipe-sfn-batch-queue/outputs.tf
+++ terraform/modules/swipe-sfn-batch-queue/outputs.tf
@@ -1,3 +1,8 @@
+output "compute_environment_security_group_id" {
+  description = "ID of the security group associated with the batch compute environments"
+  value       = aws_security_group.swipe.id
+}
+
 output "batch_spot_job_queue_arn" {
   description = "ARN of the AWS Batch Queue connected to a spot compute environment created by this module"
   value       = aws_batch_job_queue.swipe_main["spot"].arn
