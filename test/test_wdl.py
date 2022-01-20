5f9b85ed423b2f8637d8181485485d5fd6d36eae
morsecodist <todd@morsecodist.io>
2022-01-20 00:51:37 -0500

    proper test

--- test/test_wdl.py
+++ test/test_wdl.py
@@ -84,24 +84,12 @@ class TestSFNWDL(unittest.TestCase):
         self.sfn = boto3.client("stepfunctions", endpoint_url="http://localhost:8083")
         self.test_bucket = self.s3.create_bucket(Bucket="swipe-test")
         self.lamb = boto3.client("lambda", endpoint_url="http://localhost:9000")
+        self.wdl_obj = self.test_bucket.Object("test-v1.0.0.wdl")
+        self.wdl_obj.put(Body=test_wdl.encode())
+        self.input_obj = self.test_bucket.Object("input.txt")
+        self.input_obj.put(Body=test_input.encode())
 
-    def test_simple_sfn_wdl_workflow(self):
-        wdl_obj = self.test_bucket.Object("test-v1.0.0.wdl")
-        wdl_obj.put(Body=test_wdl.encode())
-        input_obj = self.test_bucket.Object("input.txt")
-        input_obj.put(Body=test_input.encode())
-        output_prefix = "out"
-        sfn_input: Dict[str, Any] = {
-          "RUN_WDL_URI": f"s3://{wdl_obj.bucket_name}/{wdl_obj.key}",
-          "OutputPrefix": f"s3://{input_obj.bucket_name}/{output_prefix}",
-          "Input": {
-              "Run": {
-                  "hello": f"s3://{input_obj.bucket_name}/{input_obj.key}",
-                  "docker_image_id": "ubuntu",
-              }
-          }
-        }
-
+    def _wait_sfn(self, sfn_input) -> None:
         execution_name = "swipe-test-{}".format(int(time.time()))
         sfn_arn = self.sfn.list_state_machines()["stateMachines"][0]["stateMachineArn"]
         res = self.sfn.start_execution(stateMachineArn=sfn_arn,
@@ -121,13 +109,44 @@ class TestSFNWDL(unittest.TestCase):
             print(event, file=sys.stderr)
 
         assert description["status"] == "SUCCEEDED", description
+
+    def test_simple_sfn_wdl_workflow(self):
+        output_prefix = "out-1"
+        self._wait_sfn({
+          "RUN_WDL_URI": f"s3://{self.wdl_obj.bucket_name}/{self.wdl_obj.key}",
+          "OutputPrefix": f"s3://{self.input_obj.bucket_name}/{output_prefix}",
+          "Input": {
+              "Run": {
+                  "hello": f"s3://{self.input_obj.bucket_name}/{self.input_obj.key}",
+                  "docker_image_id": "ubuntu",
+              }
+          }
+        })
+
         outputs_obj = self.test_bucket.Object(f"{output_prefix}/test-1/foo.txt")
         output_text = outputs_obj.get()['Body'].read().decode()
         assert output_text == "hello\nworld\nfoo\n", output_text
 
-        # foo_obj = self.test_bucket.Object(f"{output_prefix}/test-1/foo.txt")
-        # foo_text = foo_obj.get()['Body'].read().decode()
-        # assert foo_text == "foo\n", foo_text
+    def test_call_cache(self):
+        output_prefix = "out-2"
+        sfn_input: Dict[str, Any] = {
+          "RUN_WDL_URI": f"s3://{self.wdl_obj.bucket_name}/{self.wdl_obj.key}",
+          "OutputPrefix": f"s3://{self.input_obj.bucket_name}/{output_prefix}",
+          "Input": {
+              "Run": {
+                  "hello": f"s3://{self.input_obj.bucket_name}/{self.input_obj.key}",
+                  "docker_image_id": "ubuntu",
+              }
+          }
+        }
+
+        self._wait_sfn(sfn_input)
+        self.test_bucket.Object(f"{output_prefix}/test-1/out.txt").put(Body="cache_break\n".encode())
+        self._wait_sfn(sfn_input)
+
+        outputs_obj = self.test_bucket.Object(f"{output_prefix}/test-1/foo.txt")
+        output_text = outputs_obj.get()['Body'].read().decode()
+        assert output_text == "cache_break\nfoo\n", output_text
 
 
 if __name__ == "__main__":
