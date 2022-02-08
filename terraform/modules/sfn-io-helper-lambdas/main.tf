26ddb0f5f0a1cfe9497e83143fa6bf7a812b5482
morsecodist <todd@morsecodist.io>
2022-02-08 13:58:58 -0500

    fix

--- terraform/modules/sfn-io-helper-lambdas/main.tf
+++ terraform/modules/sfn-io-helper-lambdas/main.tf
@@ -169,7 +169,7 @@ resource "aws_cloudwatch_event_rule" "process_sfn_event" {
   tags = var.tags
   event_pattern = jsonencode({
     "source"      = ["aws.states"],
-    "detail-type" = "Step Functions Execution Status Change",
+    "detail-type" = ["Step Functions Execution Status Change"],
   })
 }
 
