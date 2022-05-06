a863c325c52d7808c439d2bd13c730b68e342fc0
morsecodist <todd@morsecodist.io>
2022-05-05 20:36:05 -0400

    reverse

--- terraform/modules/sfn-io-helper-lambdas/main.tf
+++ terraform/modules/sfn-io-helper-lambdas/main.tf
@@ -48,7 +48,7 @@ resource "aws_iam_role_policy" "iam_role_policy" {
 
   policy = jsonencode({
     Version : "2012-10-17",
-    Statement : concat(length(var.workspace_s3_prefixes) > 0 && var.wdl_workflow_s3_prefix == "" ? [] : [
+    Statement : concat(length(var.workspace_s3_prefixes) == 0 && var.wdl_workflow_s3_prefix == "" ? [] : [
       {
         Effect : "Allow",
         Action : [
