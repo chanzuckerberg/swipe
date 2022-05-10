cf240f9d36f31a81db00063bff7f177f520f21be
morsecodist <todd@morsecodist.io>
2022-05-10 11:26:20 -0700

    remove exp stuff

--- test/test_wdl.py
+++ test/test_wdl.py
@@ -272,7 +272,6 @@ class TestSFNWDL(unittest.TestCase):
             self.assertEqual(description["status"], "FAILED", description)
         return arn, description, messages
 
-    @unittest.skip("exp")
     def test_simple_sfn_wdl_workflow(self):
         output_prefix = "out-1"
         sfn_input: Dict[str, Any] = {
@@ -303,7 +302,6 @@ class TestSFNWDL(unittest.TestCase):
             json.loads(messages[0]["Body"])["detail"]["lastCompletedStage"], "run"
         )
 
-    @unittest.skip("exp")
     def test_failing_wdl_workflow(self):
         output_prefix = "out-fail-1"
         sfn_input: Dict[str, Any] = {
@@ -322,7 +320,6 @@ class TestSFNWDL(unittest.TestCase):
                      [-1]["executionFailedEventDetails"]["error"])
         self.assertTrue(errorType in ["UncaughtError", "RunFailed"])
 
-    @unittest.skip("exp")
     def test_staged_sfn_wdl_workflow(self):
         output_prefix = "out-2"
         sfn_input: Dict[str, Any] = {
