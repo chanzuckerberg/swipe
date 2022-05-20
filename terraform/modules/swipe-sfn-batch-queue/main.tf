6a667063ff0d8d1a7f538703c41ca36923ffafbb
David Tsai <dtsai@chanzuckerberg.com>
2022-05-20 14:06:24 -0700

    Change user data content type to individual part type

--- terraform/modules/swipe-sfn-batch-queue/main.tf
+++ terraform/modules/swipe-sfn-batch-queue/main.tf
@@ -3,7 +3,7 @@ locals {
   launch_template_swipe_user_data          = replace(file(local.launch_template_swipe_user_data_filename), "MINIWDL_DIR", var.miniwdl_dir)
   launch_template_swipe_user_data_parts = [{
     filename     = local.launch_template_swipe_user_data_filename,
-    content_type = "multipart/mixed",
+    content_type = "text/cloud-boothook",
     content      = local.launch_template_swipe_user_data
   }]
 
