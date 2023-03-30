322f578e3f5e93ef1174d8eb51b77fd973847fd2
morsecodist <todd@morsecodist.io>
2023-03-29 22:30:08 -0700

    bump

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ locals {
 module "sfn" {
   source                        = "./terraform/modules/swipe-sfn"
   app_name                      = var.app_name
-  batch_job_docker_image        = "732052188396.dkr.ecr.us-west-2.amazonaws.com/swipe:98896a737f7b16bc5e7de9901995a614fd3b4c58"
+  batch_job_docker_image        = "732052188396.dkr.ecr.us-west-2.amazonaws.com/swipe:d7932dfbea8848cc9f411dd21a1786c5f60fec99"
   batch_spot_job_queue_arn      = module.batch_queue.batch_spot_job_queue_arn
   batch_on_demand_job_queue_arn = module.batch_queue.batch_on_demand_job_queue_arn
   miniwdl_dir                   = var.miniwdl_dir
